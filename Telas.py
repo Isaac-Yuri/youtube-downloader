95e77f36a9ac5f4b71c1defbf3dd45709d13177c
Isaac Yuri <isaacyuri22@gmail.com>
2021-08-17 16:07:06 -0300

    interface da tela inicial montada

--- Telas.py
+++ Telas.py
@@ -1,4 +1,22 @@
 import PySimpleGUI as sg
 
+sg.theme('DarkGrey')
+
 def telaInicial():
-    pass
+    layout = [
+        [sg.Text('Por favor digite a URL do vídeo que deseja baixar:')],
+        [sg.Text('URL:'), sg.Input(key='url')],
+        [sg.Button('Baixar')],
+    ]
+
+    janela = sg.Window('Youtube Downloader', layout=layout)
+
+    while True:
+        eventos, valores = janela.read()
+        
+        if eventos == sg.WIN_CLOSED:
+            break
+        if eventos == 'Baixar':
+            url = valores['url']
+            print(url)
+
