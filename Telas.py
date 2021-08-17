79d712884a067fc33b919c2b7c6f33e9e0d3db7b
Isaac Yuri <isaacyuri22@gmail.com>
2021-08-17 17:22:54 -0300

    comecei a adicionar funcionalidades a tela principal

--- Telas.py
+++ Telas.py
@@ -1,12 +1,18 @@
+from typing import Text
 import PySimpleGUI as sg
 
+
 sg.theme('DarkGrey')
+
 
 def telaInicial():
     layout = [
         [sg.Text('Por favor digite a URL do vídeo que deseja baixar:')],
         [sg.Text('URL:'), sg.Input(key='url')],
+        [sg.Text('Em qual formato deseja baixar?')],
+        [sg.Checkbox('mp4', key='mp4'), sg.Checkbox('mp3', key='mp3')],
         [sg.Button('Baixar')],
+        [sg.Output(size=(30, 5),font='Arial',text_color='greenyellow')],
     ]
 
     janela = sg.Window('Youtube Downloader', layout=layout)
@@ -17,6 +23,6 @@ def telaInicial():
         if eventos == sg.WIN_CLOSED:
             break
         if eventos == 'Baixar':
+            mp4, mp3 = valores['mp4'], valores['mp3']
             url = valores['url']
-            print(url)
-
+            print(mp3, mp4)
